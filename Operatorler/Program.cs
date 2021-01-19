4ea156c80b6c048f19e58e1a129885d986905b54
onur engun <onurengun@outlook.com>
2021-01-19 21:24:21 +0300

    Add project files.

---
+++ Operatorler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sayi1 = 10;
+            double sayi2 = 15;
+            double toplam = sayi1 + sayi2;
+            Console.WriteLine(toplam);
+
+            double fark = sayi1 - sayi2;
+            Console.WriteLine(fark);
+
+            double carpim = sayi1 * sayi2;
+            Console.WriteLine(carpim);
+
+            //bir tamsayının başka bir tamsayıya bölümü tamsayı verir
+            double bolum = sayi1 / sayi2;
+            Console.WriteLine(bolum);
+        }
+    }
+}
